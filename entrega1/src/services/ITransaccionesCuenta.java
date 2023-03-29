9a5872d1efd66e56a07452bb195290c53c878f6b
IsmeniaIMGO <ismeniam.guevarao@uqvirtual.edu.co>
2023-03-29 11:44:38 -0500

    uml

--- entrega1/src/services/ITransaccionesCuenta.java
+++ entrega1/src/services/ITransaccionesCuenta.java
@@ -2,11 +2,11 @@ package services;
 
 public interface ITransaccionesCuenta {
 
-	public void ConsignarDinero(String id, String cedula, int cantidad) throws Exception;
+	public void consignarDinero(String id, String cedula, int cantidad) throws Exception;
 	
-	public void TransferirDinero(String idDestino, int cantidad, String idOrigen, String clave) throws Exception;
+	public void transferirDinero(String idDestino, int cantidad, String idOrigen, String clave) throws Exception;
 	
-	public void RetirarDinero(String cedula, String id, int cantidad, String clave) throws Exception;
+	public void retirarDinero(String cedula, String id, int cantidad, String clave) throws Exception;
 	
 	public int consultarSaldo(String id) throws Exception;
 	
